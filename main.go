03c5bb62a0adf5544948b364158d57cf3e5d0039
abhpc <xyliucd@foxmail.com>
2024-09-07 10:16:54 +0800

    bash auto update

--- main.go
+++ main.go
@@ -2030,9 +2030,8 @@ func (s *serverConfig) GetClusterInfo(ctx context.Context, in *pb.GetClusterInfo
 		// fullCmd := getPartitionStatusCmd + " --format='%P %c %C %G %a %D %F'| tr '\n' ','"
 
 		//sinfo -N --nohead --format='%P %c %C %G %a %D %F %n'|awk '!seen[$NF]++'|grep '^MX'| tr '\n' ','
-		getPartitionStatusCmd1 := fmt.Sprintf("sinfo -N --nohead")
-		getPartitionStatusCmd2 := fmt.Sprintf("|grep '^%s'| tr '\n' ','", v)
-		fullCmd := getPartitionStatusCmd1 + "--format='%P %c %C %G %a %D %F %n'|awk '!seen[$NF]++'" + getPartitionStatusCmd2
+		getPartitionStatusCmd := fmt.Sprintf("|grep '^%s'| tr '\n' ','", v)
+		fullCmd := "sinfo -N --nohead --format='%P %c %C %G %a %D %F %n'|awk '!seen[$NF]++'" + getPartitionStatusCmd
 
 		result, err := utils.RunCommand(fullCmd) // 状态
 		if err != nil || utils.CheckSlurmStatus(result) {
