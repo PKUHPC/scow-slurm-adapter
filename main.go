b0fd6fdb8b4ca66d7dd91bbea718fc660daea87e
abhpc <xyliucd@foxmail.com>
2024-09-07 10:40:43 +0800

    bash auto update

--- main.go
+++ main.go
@@ -2026,12 +2026,12 @@ func (s *serverConfig) GetClusterInfo(ctx context.Context, in *pb.GetClusterInfo
 		var noAvailableNodes int
 
 		// sinfo -p %s --noheader --format='%P %c %C %G %a %D %F'| tr '\n' ','
-		//getPartitionStatusCmd := fmt.Sprintf("sinfo -p %s --noheader", v)
-		//fullCmd := getPartitionStatusCmd + " --format='%P %c %C %G %a %D %F'| tr '\n' ','"
+		getPartitionStatusCmd := fmt.Sprintf("sinfo -p %s --noheader", v)
+		fullCmd := getPartitionStatusCmd + " --format='%P %c %C %G %a %D %F'| tr '\n' ','"
 
 		//sinfo -N --nohead --format='%P %c %C %G %a %D %F %n'|awk '!seen[$NF]++'|grep '^MX'| tr '\n' ','
-		getPartitionStatusCmd := fmt.Sprintf("|grep '^%s'|awk '!seen[$NF]++{print $1,$2,$3,$4,$5,$6,$7}'| tr '\n' ','", v)
-		fullCmd := "sinfo -N --nohead --format='%P %c %C %G %a %D %F %n'" + getPartitionStatusCmd
+		//getPartitionStatusCmd := fmt.Sprintf("|grep '^%s'|awk '!seen[$NF]++{print $1,$2,$3,$4,$5,$6,$7}'| tr '\n' ','", v)
+		//fullCmd := "sinfo -N --nohead --format='%P %c %C %G %a %D %F %n'" + getPartitionStatusCmd
 
 		result, err := utils.RunCommand(fullCmd) // 状态
 		if err != nil || utils.CheckSlurmStatus(result) {
