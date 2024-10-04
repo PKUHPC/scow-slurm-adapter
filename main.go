4a33f508e88d146f37570a1a93438946bf0e1ea9
abhpc <xyliucd@foxmail.com>
2024-10-04 21:37:34 +0800

    noAvailableGpus = 0

--- main.go
+++ main.go
@@ -2074,6 +2074,7 @@ func (s *serverConfig) GetClusterInfo(ctx context.Context, in *pb.GetClusterInfo
 			noAvailableCores = noAvailableCores + noAvailableCoresTmp
 			// fmt.Println("Partition Element:", partitionElement)
 			gpuInfo := resultList[3] // 这是gpu的信息
+			noAvailableGpus = 0
 			if gpuInfo == "(null)" {
 				runningGpus = 0
 				idleGpus = 0
