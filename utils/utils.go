2352e2637f6a4cf46b55dddd4a7b5f9baa184810
weihong.xu <xuweihong.cn@gmail.com>
2024-11-01 10:46:15 +0800

    fix: 修改合法账号名模式

--- utils/utils.go
+++ utils/utils.go
@@ -338,8 +338,8 @@ func GetUserNameByUid(uid int) (string, error) {
 
 // 判断字符串是否为合法用户名或账号名
 func CheckAccountOrUserStrings(s string) bool {
-	// 允许字母、数字、下划线、连字符
-	pattern := "^[a-zA-Z0-9_-]+$"
+	// 允许小写字母、数字、下划线、连字符
+	pattern := "^[a-z0-9_-]+$"
 	// 编译正则表达式
 	reg := regexp.MustCompile(pattern)
 	// 使用正则表达式判断字符串是否符合模式
